8d9bab0274d0f183b8f65ec76ac29228e0783e7c
Ali Colver <me@alicolver.com>
2024-12-09 00:48:52 +0000

    day8

--- calendar/src/08/Day8.hs
+++ calendar/src/08/Day8.hs
@@ -21,8 +21,10 @@ day8 = do
     let gridWithIndex = mapToPoint grid
     let antennas = concatMap (filter isAntenna) gridWithIndex
     let bounds = Bounds (length gridWithIndex) (length (head gridWithIndex))
-    print (length (nub (concatMap (addPoints antennas [] bounds False) (concat gridWithIndex))))
-    print (length (nub (concatMap (addPoints antennas [] bounds True) (concat gridWithIndex))))
+    print (map (solve antennas bounds) [True, False])
+
+solve :: [Point] -> Bounds -> Bool -> Int
+solve antennas bounds isP1 = length (nub (concatMap (addPoints antennas [] bounds isP1) antennas))
 
 isAntenna :: Point -> Bool
 isAntenna (Point v _) = v /= '.'
@@ -33,7 +35,10 @@ addPoints (p2:p2s) x bs isP1 p1
     | inline p1 p2 = addPoints p2s (x ++ nps) bs isP1 p1
     | otherwise = addPoints p2s x bs isP1 p1
     where
-        nps = if isP1 then addNewPoints p1 p2 else addAllPointsOnLine p1 (getGradient p1 p2) bs
+        nps = if isP1 then p1Sol else p2Sol
+        p1Sol = addPoint p1 (-g1,-g2) bs ++ addPoint p2 (g1,g2) bs
+        p2Sol = addAllPointsOnLine p1 (getGradient p1 p2) bs
+        (g1,g2) = getGradient p1 p2
 
 inline :: Point -> Point -> Bool
 inline (Point c1 (x1,y1)) (Point c2 (x2,y2)) = c1 == c2 && x1 /= x2 && y1 /= y2
@@ -41,8 +46,18 @@ inline (Point c1 (x1,y1)) (Point c2 (x2,y2)) = c1 == c2 && x1 /= x2 && y1 /= y2
 getGradient :: Point -> Point -> (Int, Int)
 getGradient (Point _ (x1,y1)) (Point _ (x2,y2)) = (x2 - x1, y2 - y1)
 
+addPoint :: Point -> (Int,Int) -> Bounds -> [Point]
+addPoint (Point _ (x1,y1)) (x2,y2) bs
+    | inGrid bs np = [np]
+    | otherwise = []
+    where
+        np = Point '+' (x1+x2, y1+y2)
+
 addAllPointsOnLine :: Point -> (Int, Int) -> Bounds -> [Point]
-addAllPointsOnLine (Point _ (x, y)) (x1,y1) bs = addGrad (x,y) (x1,y1) [] bs ++ addGrad (x,y) (- x1,- y1) [] bs
+addAllPointsOnLine (Point _ (x, y)) (x1,y1) bs = pos ++ neg
+    where
+        pos = addGrad (x,y) (x1,y1) [] bs
+        neg = addGrad (x,y) (- x1,- y1) [] bs
 
 addGrad :: (Int, Int) -> (Int, Int) -> [Point] -> Bounds -> [Point]
 addGrad (x1, y1) (x2, y2) ps bs
@@ -50,16 +65,6 @@ addGrad (x1, y1) (x2, y2) ps bs
     | otherwise = ps
     where
         np = Point '+' (x1+x2, y1+y2)
-
-addNewPoints :: Point -> Point -> [Point]
-addNewPoints (Point _ (x1, y1)) (Point _ (x2, y2))
-    | x1 == x2 = [Point '+' (x1, y1 - abs (y2 - y1)), Point '+' (x1, y2 + abs (y2 - y1))]
-    | y1 == y2 = [Point '+' (x1 - abs (x1 - x2), y1), Point '+' (x2 + abs (x1 - x2), y2)]
-    | y2 < y1 = []
-    | otherwise =
-        if x2 < x1
-            then [Point '+' (x2 - abs (x1 - x2), y2 + abs (y1 - y2)), Point '+' (x1 + abs (x1 - x2), y1 - abs (y1 - y2))]
-            else [Point '+' (x1 - abs (x1 - x2), y1 - abs (y1 - y2)), Point '+' (x2 + abs (x1 - x2), y2 + abs (y1 - y2))]
 
 mapToPoint :: [[Char]] -> [[Point]]
 mapToPoint matrix = map (map (uncurry Point)) (mapWithIndices matrix)
